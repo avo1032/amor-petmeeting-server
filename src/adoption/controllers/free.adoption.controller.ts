58e456a5dab466423e9364a053ae85956dc03eee
유승완 <swan1032@naver.com>
2024-05-06 01:13:22 +0900

    refactor: createFreeAdoption

--- src/adoption/controllers/free.adoption.controller.ts
+++ src/adoption/controllers/free.adoption.controller.ts
@@ -3,6 +3,8 @@ import { FreeAdoptionService } from '../services/free.adoption.service';
 import { CreateFreeAdoptionDto } from '../dto/req.free.adoption.dto';
 import { FormDataRequest } from 'nestjs-form-data';
 import { AccessTokenGuard } from 'src/common/guards/access.token.guard';
+import { UserInfo } from 'src/common/decorators/user.info';
+import { User } from '@prisma/client';
 
 @Controller('free-adoption')
 export class FreeAdoptionController {
@@ -11,7 +13,11 @@ export class FreeAdoptionController {
   @UseGuards(AccessTokenGuard)
   @Post()
   @FormDataRequest()
-  async createFreeAdoption(@Body() body: CreateFreeAdoptionDto) {
-    // return this.freeAdoptionService.createFreeAdoption(body);
+  async createFreeAdoption(
+    @Body() body: CreateFreeAdoptionDto,
+    @UserInfo() user: User,
+  ) {
+    console.log(user);
+    return this.freeAdoptionService.createFreeAdoption(body, user);
   }
 }
--- src/adoption/services/free.adoption.service.ts
+++ src/adoption/services/free.adoption.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateFreeAdoptionDto } from '../dto/req.free.adoption.dto';
 import { createUUIDv4 } from 'src/common/utils/create.uuid';
 import { AwsS3Service } from 'src/aws/services/aws.s3.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class FreeAdoptionService {
@@ -11,28 +12,29 @@ export class FreeAdoptionService {
     private readonly awsS3Service: AwsS3Service,
   ) {}
 
-  // async createFreeAdoption(body: CreateFreeAdoptionDto) {
-  //   const { subImages, mainImage, ...postData } = body;
-  //   const mainImageKey = `freeAdoption/${createUUIDv4()}`;
-  //   const subImageKeys = subImages.map(() => `freeAdoption/${createUUIDv4()}`);
-  //   const mainImageUrl = await this.awsS3Service.uploadFile(
-  //     mainImage,
-  //     mainImageKey,
-  //   );
-  //   const subImageUrls = await Promise.all(
-  //     subImages.map((image, index) =>
-  //       this.awsS3Service.uploadFile(image, subImageKeys[index]),
-  //     ),
-  //   );
-  //   return this.prisma.freeAdoptionPost.create({
-  //     data: {
-  //       uuid: createUUIDv4(),
-  //       ...postData,
-  //       mainImage: mainImageUrl.Location,
-  //       subImages: {
-  //         create: subImageUrls.map((image) => ({ image: image.Location })),
-  //       },
-  //     },
-  //   });
-  // }
+  async createFreeAdoption(body: CreateFreeAdoptionDto, user: User) {
+    const { subImages, mainImage, ...postData } = body;
+    const mainImageKey = `freeAdoption/${createUUIDv4()}`;
+    const subImageKeys = subImages.map(() => `freeAdoption/${createUUIDv4()}`);
+    const mainImageUrl = await this.awsS3Service.uploadFile(
+      mainImage,
+      mainImageKey,
+    );
+    const subImageUrls = await Promise.all(
+      subImages.map((image, index) =>
+        this.awsS3Service.uploadFile(image, subImageKeys[index]),
+      ),
+    );
+    return this.prisma.freeAdoptionPost.create({
+      data: {
+        userId: user.id,
+        uuid: createUUIDv4(),
+        ...postData,
+        mainImage: mainImageUrl.Location,
+        subImages: {
+          create: subImageUrls.map((image) => ({ image: image.Location })),
+        },
+      },
+    });
+  }
 }
